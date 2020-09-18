d4f5911f1ce1a6ba9adea282530d735bd9676d06
louise <louise87276@gmail.com>
2020-09-18 12:49:32 +0800

    角色能力值 測試

---
+++ Assets/CharacterStats.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    //public GetHitEffect getHitEffect;
+    public float maxHealth = 100f;
+    public float currentHealth { get; private set; }
+    public Stats Damage;
+    public Stats Armor;
+
+    void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+    public void TakeDamage(float Damage)
+    {
+        Damage -= Armor.GetValue();
+        Damage = Mathf.Clamp(Damage, 0, float.MaxValue);
+
+        currentHealth -= Damage;
+        Debug.Log(transform.name + "被攻擊造成 " + Damage + " 傷害,剩餘 " + currentHealth + " 點生命");
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+    public virtual void Die()
+    {
+        Debug.Log(transform.name + " 被擊敗");
+    }
+}
---
+++ Assets/Stats.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Stats : MonoBehaviour
+{
+    int baseValue;
+
+    List<float> modifiers = new List<float>();
+    public int GetValue()
+    {
+        float FinalValue = baseValue;
+        modifiers.ForEach(x => FinalValue += x);
+        return (int)FinalValue;
+    }
+    public void AddModifiers(float modifier)
+    {
+        if (modifier != 0)
+        {
+            modifiers.Add(modifier);
+        }
+    }
+    public void RemoveModifiers(float modifier)
+    {
+        if (modifier != 0)
+        {
+            modifiers.Remove(modifier);
+        }
+    }
+}

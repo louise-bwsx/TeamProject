ad747b0d54845f4b5185d6d934f490c763f6771b
Home\louise <louise87276@gmail.com>
2020-09-19 14:07:08 +0800

    主介面互動完成，設定、存檔介面待實作

---
+++ Assets/02_Scripts/GameMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+    public GameObject[] anyWindow = new GameObject[3];
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            EscButton();
+        }
+    }
+    public void EscButton()
+    {
+        if (!anyWindow[0].activeSelf)
+        {
+            //for所有的視窗SetActive(false)
+            foreach (GameObject window in anyWindow)
+            {
+                window.SetActive(false);
+            }
+
+            anyWindow[0].SetActive(true);
+        }
+        else
+        {
+            anyWindow[0].SetActive(false);
+        }
+    }
+    public void GameContinue()
+    {
+        anyWindow[0].SetActive(false);
+    }
+    public void QuitGame()
+    {
+        SceneManager.LoadScene(0) ;
+        //聽說build出來真的會結束但是在unity裡面不會有任何作用所以用debug.log來代替
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}

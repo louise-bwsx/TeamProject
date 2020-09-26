14a8adc619e2c624aca05c078bec52d8001870b4
Home\louise <louise87276@gmail.com>
2020-09-26 10:03:39 +0800

    修改怪物掉落物icon

---
+++ Assets/02_Scripts/EnemyController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float lookRaduis = 10;
+    Transform target;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+        if (distance <= lookRaduis)
+        {
+            agent.SetDestination(target.position);
+            if (distance <= agent.stoppingDistance)
+            {
+                //攻擊目標
+                //面對目標 這裡要改z軸面對攝影機
+                FaceTarget();
+            }
+        }
+    }
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation,lookRotation,Time.deltaTime *5f);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRaduis);
+    }
+}
---
+++ Assets/02_Scripts/Stats.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class Stats : MonoBehaviour
+{
+    float baseValue;
+
+    List<float> modifiers = new List<float>();
+    public int GetValue()
+    {
+        Debug.Log(1);
+        float FinalValue = baseValue;
+        modifiers.ForEach(x => FinalValue += x);
+        return (int)FinalValue;
+    }
+    public void AddModifiers(float modifier)
+    {
+        if (modifier != 0)
+        {
+            modifiers.Add(modifier);
+        }
+    }
+    public void RemoveModifiers(float modifier)
+    {
+        if (modifier != 0)
+        {
+            modifiers.Remove(modifier);
+        }
+    }
+}

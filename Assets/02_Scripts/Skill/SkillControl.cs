8ab901a628da5726c81d67517b817ee0db86f3da
DESKTOP-NO85EN4\SKY <a198910547@gmail.com>
2020-10-02 19:41:20 +0800

    已可以滑鼠滾輪 搭配中鍵 技能發招 冷卻圖示待修復

---
+++ Assets/02_Scripts/Skill/SkillControl.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillControl : MonoBehaviour
+{
+    [SerializeField]
+    public Button[] list;
+
+    public SkillF5Launch F5Skill;
+    public SkillF6Launch F6Skill;
+    public SkillF8Launch F8Skill;
+
+    public int CurIdx = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Mouse ScrollWheel");
+        if (move > 0.0f)
+        {
+
+            if (CurIdx > 0) CurIdx--;
+            SetPos();
+        }
+        else if (move < 0.0f)
+        {
+            if (CurIdx < list.Length - 1) CurIdx++;
+            SetPos();
+        }
+        //滑鼠左鍵的效果
+        if (Input.GetMouseButtonDown(2))
+        {
+            Button b = list[CurIdx];
+            if (CurIdx == 0)
+            {
+                F5Skill.shoot();
+            }
+            if (CurIdx == 1)
+            {
+                F6Skill.shoot();
+            }
+            if (CurIdx == 3)
+            {
+                F8Skill.shoot();
+            }
+        }
+    }
+    public void SetPos()
+    {
+        for (int i = 0; i < list.Length; i++)
+        {
+            if (i == CurIdx)
+                list[i].enabled = true;
+            else
+                list[i].enabled = false;
+        }
+
+
+    }
+}

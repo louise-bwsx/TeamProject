22d2cea6b98649a28dc522695ce7b744203e8038
Home\louise <louise87276@gmail.com>
2020-10-09 22:21:35 +0800

    boss放置完成

---
+++ Assets/02_Scripts/Door.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Transform door;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log(1);
+            //物體軸的x
+            door.Translate(Vector3.up * -5f);
+        }
+    }
+}

818e67e9b53ad106f3c3d5057f7ceda289922e2b
Laptop/Louise <louise87276@gmail.com>
2020-10-13 16:41:02 +0800

    測試

--- Assets/02_Scripts/PlayerFaceDirection.cs
+++ Assets/02_Scripts/PlayerFaceDirection.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class PlayerFaceDirection : MonoBehaviour
 {
     public PlayerControl playerControl;
-    public LookAtCursor lookAtCursor;
+    public Transform playerRotation;
     void Update()
     {
         if (playerControl.cantMove == false && Time.timeScale != 0)
-        { 
-            if (lookAtCursor.rotationangle.y > 0)
+        {
+            if (playerRotation.localEulerAngles.y < 180 && playerRotation.localEulerAngles.y > 0)
             {
-                transform.rotation = Quaternion.Euler(30, 270, 0);
+                transform.rotation = Quaternion.Euler(-30, 90, 0);
             }
             else
             {
-                transform.rotation = Quaternion.Euler(-30, 90, 0);
-            }        
+                transform.rotation = Quaternion.Euler(30, 270, 0);
+            }
         }
     }
 }

1da2728c04fe3f2fa2d85acd07f4977ae1dd587d
Laptop/Louise <louise87276@gmail.com>
2020-10-17 18:18:37 +0800

    測試

--- Assets/02_Scripts/DetectMonster.cs
+++ Assets/02_Scripts/DetectMonster.cs
@@ -7,6 +7,7 @@ public class DetectMonster : MonoBehaviour
     GameObject door;
     void Start()
     {
+        //之後改成public 避免抓到從boss戰回來的傳送門
         door = GameObject.FindGameObjectWithTag("Door");
     }
 
--- Assets/02_Scripts/Enemy/BossHealth.cs
+++ Assets/02_Scripts/Enemy/BossHealth.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class BossHealth : MonsterHealth
 {
+    public Transform minionsGroup;
     public override void MonsterDead()
     {
         base.MonsterDead();
     }
     public override void OnTriggerEnter(Collider other)
     {
-        //當boss血量在maxHp時才能用正常手段攻擊
-        if (Hp > maxHp * 0.7)
-        { 
+        //Boss血量第一階段扣血條件
+        if (Hp > maxHp * 0.7 && minionsGroup.childCount == 0)
+        {
+            base.OnTriggerEnter(other);
+        }
+        //Boss血量第二階段 當雕像打爆以後會繞過碰觸機制直接GetHit Boss
+
+        //Boss血量第三階段此時Boss開始會放大招
+        if (Hp < maxHp * 0.3)
+        {
             base.OnTriggerEnter(other);
         }
     }

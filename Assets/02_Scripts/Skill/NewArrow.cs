2976cc7fa18152b8957df37d0dcd187366772510
Laptop/Louise <louise87276@gmail.com>
2020-10-27 11:11:54 +0800

    shader調整 技能碰撞特效新增 教學圖片修正

--- Assets/02_Scripts/Skill/NewArrow.cs
+++ Assets/02_Scripts/Skill/NewArrow.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class NewArrow : MonoBehaviour
 {
+    public GameObject hitEffect;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Skill") || other.CompareTag("Wall") || other.CompareTag("Player"))
         {
+            GameObject FX = Instantiate(hitEffect, transform.position, transform.rotation);
+            Destroy(FX);
             Destroy(gameObject);
         }
     }

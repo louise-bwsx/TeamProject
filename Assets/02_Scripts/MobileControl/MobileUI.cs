7ed7d593ce96e6cf6e7b6ec5c11e7904238fc869
Laptop/Louise <louise87276@gmail.com>
2020-11-06 16:45:31 +0800

    手機版測試

---
+++ Assets/02_Scripts/MobileControl/MobileUI.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileUI : MonoBehaviour
+{
+    public void UIEnable(Transform typeUI)
+    {
+        typeUI.gameObject.SetActive(!typeUI.gameObject.activeSelf);
+    }
+}

a28e824679f6c6d5df3a62f132570f002a582906
Home\louise <louise87276@gmail.com>
2020-11-13 17:25:34 +0800

    新增毒技能 以及 冷卻時間修正 如果特別嚴重的bug我就要去做新的專題了

--- Assets/02_Scripts/Skill/RemoteSkillPosition.cs
+++ Assets/02_Scripts/Skill/RemoteSkillPosition.cs
@@ -6,31 +6,30 @@ using UnityEngine.UI;
 public class RemoteSkillPosition : MonoBehaviour
 {
     public float skillControlSpeed;
-    public Transform skillPosition;
+    public Transform playerSprite;
+    public Transform mousePosition;
     public RectTransform hadle;
     public RectTransform outerCircle;
     public MeshRenderer meshRenderer;
     public Animator animator;
-
     public bool isTouch;
+    public MobileSkillChoose mobileSkillChoose;
+
     int floor;
     Vector3 originPosition;
-    MobileSkillChoose mobileSkillChoose;
-    MobileAttack mobileAttack;
 
     void Start()
     {
-        mobileAttack = FindObjectOfType<MobileAttack>();
         mobileSkillChoose = FindObjectOfType<MobileSkillChoose>();
-        meshRenderer = GetComponent<MeshRenderer>();
         floor = LayerMask.GetMask("Floor");
     }
     void FixedUpdate()
     {
-        if (isTouch)
+        //範圍鎖定失敗
+        if (isTouch /*&& Vector3.Distance(transform.position,playerSprite.position)< maxDistence*/)
         {
             animator.SetBool("IsCheck", !isTouch);
-            meshRenderer.enabled = isTouch;
+            //meshRenderer.enabled = isTouch;
             //用一個Vector3把搖桿距離中心點的位置給存起來
             Vector3 offset = outerCircle.position - hadle.position;
             //因為搖桿是Vector2所以要把它的Y給Z
@@ -38,30 +37,38 @@ public class RemoteSkillPosition : MonoBehaviour
             //算距離?
             Vector3 direction = Vector3.ClampMagnitude(offset, 1f);
             //技能施放位置移動
-            skillPosition.Translate(direction * -1f * skillControlSpeed * Time.deltaTime);
+            mousePosition.Translate(direction * -1f * skillControlSpeed * Time.deltaTime);
             RaycastHit hit;
             //技能施放位置朝地板打射線如果碰到地板
-            if (Physics.Raycast(skillPosition.position, -skillPosition.up, out hit, 10f, floor))
+            if (Physics.Raycast(mousePosition.position, -mousePosition.up, out hit, 10f, floor))
             {
                 //先存起來等不小心跑進地板再拿出來用
-                originPosition = skillPosition.position;
+                originPosition = mousePosition.position;
                 //技能位置等於射線打到的點朝上0.1f
-                skillPosition.position = hit.point + skillPosition.up * 0.1f;
+                mousePosition.position = hit.point + mousePosition.up * 0.1f;
             }
             //如果潛入地下就回到還沒回到地下以前的點
             else
             {
-                skillPosition.position = originPosition;
+                mousePosition.position = originPosition;
             }
         }
     }
     public void SetSkillPosition(bool isTouch)
     {
-        this.isTouch = isTouch;
+        if (mobileSkillChoose.skillList[0].skillTimer < mobileSkillChoose.skillList[0].skillCD)
+        {
+            meshRenderer.enabled = false;
+        }
+        else
+        {
+            meshRenderer.enabled = true;
+        }
         //為了讓每次按下技能時確保從角色底下出現而不是上一個施放位置
         if (isTouch)
         {
-            skillPosition.position = skillPosition.parent.position;
+            mousePosition.position = mousePosition.parent.position;
         }
+        this.isTouch = isTouch;
     }
 }

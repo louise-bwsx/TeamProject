4095d19ceda4365c3631d6d4dba6744ad08f14c1
PC-Home <louise87276@gmail.com>
2022-10-30 20:52:14 +0800

    mac沒辦法pull 重新創一個專案試試

--- Assets/01_Project/Test/RoomGenerator.cs
+++ Assets/01_Project/Test/RoomGenerator.cs
@@ -13,7 +13,7 @@ public class RoomGenerator : MonoBehaviour
     /// </summary>
     public int roomNumber;
     public Color startColor, endColor;
-    private GameObject endRoom;
+    private GameObject startRoom, endRoom;
 
     [Header("位置控制")]
     public Transform generatorPoint;
@@ -22,40 +22,25 @@ public class RoomGenerator : MonoBehaviour
     public LayerMask roomLayer;
 
     public List<Room> rooms = new List<Room>();
+
     void Start()
     {
         xOffset = 1;
         yOffset = 1;
 
-        //xOffset = 0;
-        //yOffset = 0;
-        //Debug.Log(xOffset);
-        //Debug.Log(yOffset);
         Reborn();
 
-        //rooms[0].GetComponent<SpriteRenderer>().color = startColor;
-
-        //endRoom = rooms[0].gameObject;
-        //找到最後房間
+        rooms[0].GetComponent<SpriteRenderer>().color = Color.yellow;
+        startRoom = rooms[0].gameObject;
         foreach (var room in rooms)
         {
-            //if (room.transform.position.sqrMagnitude > endRoom.transform.position.sqrMagnitude)
-            //{
-            //    endRoom = room.gameObject;
-            //}
-
             SetupRoom(room, room.transform.position);
         }
-        //endRoom.GetComponent<SpriteRenderer>().color = endColor;
+        FindEndRoom();
+        endRoom = rooms[rooms.Count - 1].gameObject;
+        endRoom.GetComponent<SpriteRenderer>().color = endColor;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //if(Input.anyKeyDown){
-        //    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        //}
-    }
     /// <summary>
     /// 生成房間
     /// </summary>
@@ -69,32 +54,9 @@ public class RoomGenerator : MonoBehaviour
             ChangePointPos(rooms[rooms.Count - 1]);
         }
     }
+
     public void ChangePointPos(Room newRoom)
     {
-        //direction = (Direction)Random.Range(0, 4);
-        //switch (direction)
-        //{
-        //    case Direction.up:
-        //        generatorPoint.position += new Vector3(0, yOffset, 0);
-        //        break;
-        //    case Direction.down:
-        //        generatorPoint.position += new Vector3(0, -yOffset, 0);
-        //        break;
-        //    case Direction.left:
-        //        generatorPoint.position += new Vector3(-xOffset, 0, 0);
-        //        break;
-        //    case Direction.right:
-        //        generatorPoint.position += new Vector3(xOffset, 0, 0);
-        //        break;
-        //}
-        //// 11時偶爾近來有些重疊
-        //// 00時一定進來而且都在原點
-        //if (Physics2D.OverlapCircle(generatorPoint.position, 0.2f, roomLayer))
-        //{
-        //    Debug.Log("if");
-        //    generatorPoint.position += new Vector3(0, yOffset, 0);
-        //}
-
         // 11時一定會進來不會很分散
         // 00時Unity直接卡住
         do
@@ -127,5 +89,74 @@ public class RoomGenerator : MonoBehaviour
         newRoom.roomRight = Physics2D.OverlapCircle(roomPosition + new Vector3(xOffset, 0, 0), 0.2f, roomLayer);
 
         //newRoom.UpdateRoom();
+    }
+
+    public void FindEndRoom()
+    {
+        Room farRoom = null;
+        int index = 0;
+        for (var i = 0; i < rooms.Count; i++)
+        {
+            if (rooms[i].doorNumber > 1)
+            {
+                continue;
+            }
+            if (!farRoom)
+            {
+                farRoom = rooms[i];
+                index = i;
+                continue;
+            }
+            float distance = Vector3.Distance(startRoom.transform.position, rooms[i].transform.position);
+            float checkDistance = Vector3.Distance(startRoom.transform.position, farRoom.transform.position);
+            if (distance > checkDistance)
+            {
+                farRoom = rooms[i];
+                index = i;
+                continue;
+            }
+        }
+        Vector3 farestPosition = farRoom.transform.position;
+        Destroy(rooms[index].gameObject);
+        rooms[index] = Instantiate(roomRrefab[bossRoom], farestPosition, Quaternion.identity).GetComponent<Room>();
+        farRoom.GetComponent<SpriteRenderer>().color = Color.red;
+        //最大數值
+        // for (int i = 0; i < rooms.Count; i++)
+        // {
+        //     if (rooms[i].stepToStart > maxStep)
+        //     {
+        //         maxStep = rooms[i].stepToStart;
+        //     }
+        // }
+
+        // //獲得最大值的房間和次大值
+        // foreach (var room in rooms)
+        // {
+        //     if (room.stepToStart == maxStep)
+        //         farRoom.Add(room.gameObject);
+        //     if (room.stepToStart == maxStep - 1)
+        //         lessFarRooms.Add(room.gameObject);
+        // }
+
+        // for (int i = 0; i < farRoom.Count; i++)
+        // {
+        //     if (farRoom[i].GetComponent<Room>().doorNumber == 1)
+        //         oneWayRooms.Add(farRoom[i]);
+        // }
+
+        // for (int i = 0; i < lessFarRooms.Count; i++)
+        // {
+        //     if (lessFarRooms[i].GetComponent<Room>().doorNumber == 1)
+        //         oneWayRooms.Add(lessFarRooms[i]);
+        // }
+
+        // if (oneWayRooms.Count != 0)
+        // {
+        //     endRoom = oneWayRooms[Random.Range(0, oneWayRooms.Count)];
+        // }
+        // else
+        // {
+        //     endRoom = farRoom[Random.Range(0, farRoom.Count)];
+        // }
     }
 }
